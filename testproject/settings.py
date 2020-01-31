c337737f89e9a78e2a3bc55ea4a12742e2d8c2b5
Volodymyr <iaremenko.volodymyr@gmail.com>
2020-02-01 01:40:16 +0200

    Mergin commit

--- testproject/settings.py
+++ testproject/settings.py
@@ -79,9 +79,8 @@ DATABASES = {
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST']
-        'PORT':'' ,
-        ),
+        'HOST': os.environ['DB_HOST'],
+        'PORT':['DB_PORT'] ,
     }
 }
 
