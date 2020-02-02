9cb7e23fe2af2a33e162ec26ab575aead2a98ba9
Volodymyr <iaremenko.volodymyr@gmail.com>
2020-02-03 00:42:01 +0200

    Change index template

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path ('', views.index, name='index'),
+        path('', views.index, name='index'),
+        path('<int:quetion_id>/', views.detail, name='detail'),
+        path('<int:quetion_id>/results/', views.result, name='results'),
+        path('<int:qustion_id>/vote/', views.vote, name='vote'),
         ]
