25d06160ddc2ac10edc7eb793988e99c6a4eaf7c
Volodymyr <iaremenko.volodymyr@gmail.com>
2020-02-06 13:43:02 +0200

    Use generic.views

--- polls/views.py
+++ polls/views.py
@@ -23,30 +23,10 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
-#
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404('Question not found')
-#     return render(request, 'polls/detail.html', {'question':question})
-#
-# def result(request, question_id):
-#     qustion = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': qustion})
-#
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question':question,
