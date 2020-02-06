e3c48eac80b0d03f8c73399174e11d8642f4bd07
Volodymyr <iaremenko.volodymyr@gmail.com>
2020-02-06 11:52:19 +0200

    Amend views

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name='polls'
 urlpatterns = [
-        path('', views.index, name='index'),
-        path('<int:question_id>/', views.detail, name='detail'),
-        path('<int:question_id>/results/', views.result, name='results'),
+        path('', views.IndexView, name='index'),
+        path('<int:question_id>/', views.DetailView, name='detail'),
+        path('<int:question_id>/results/', views.ResultsView, name='results'),
         path('<int:question_id>/vote/', views.vote, name='vote'),
         ]
